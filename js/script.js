54d3b5131b61fd42ce0f02171a2c0b938ec39096
Dmitriy-Rassol <rassoldima@yandex.ru>
2019-11-06 01:54:06 +0300

    Последняя попытка

--- js/script.js
+++ js/script.js
@@ -6,53 +6,53 @@ var form = popup.querySelector(".feedback-form");
 var login = popup.querySelector("[name=name]");
 var email = popup.querySelector("[name=email]");
 var isStorageSupport = true;
-var storage = '';
+var storage = "";
 
 try {
-    storage = localStorage.getItem("login");
-} catch (err) {
+  storage = localStorage.getItem("login");
+}
+catch (err) {
     isStorageSupport = false;
 }
-
 btn.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.add("popup-show");
-    if (storage) {
-        login.value = storage;
-        email.focus();
-    } else {
-        login.focus();
-    }
-    overlay.classList.add("page-overlay-show");
+  evt.preventDefault();
+  popup.classList.add("popup-show");
+  if (storage) {
+    login.value = storage;
+    email.focus();
+  } else {
+    login.focus();
+  }
+  overlay.classList.add("page-overlay-show");
 });
 close.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.remove("popup-show");
-    popup.classList.remove("modal-error");
-    overlay.classList.remove("page-overlay-show");
+  evt.preventDefault();
+  popup.classList.remove("popup-show");
+  popup.classList.remove("modal-error");
+  overlay.classList.remove("page-overlay-show");
 });
 
 form.addEventListener("submit", function (evt) {
-    if (!login.value || !email.value) {
-        evt.preventDefault();
-        popup.classList.remove("modal-error");
-        popup.offsetWidth = popup.offsetWidth;
-        popup.classList.add("modal-error");
-        console.log("Нужно ввести свои данные");
-    }else {
-        if (isStorageSupport) {
-            localStorage.setItem("login", login.value);
-        }
+  if (!login.value || !email.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+    console.log("Нужно ввести свои данные");
+  }else {
+    if (isStorageSupport) {
+      localStorage.setItem("login", login.value);
     }
+  }
 });
 
 window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-        evt.preventDefault();
-        if (popup.classList.contains("popup-show")) {
-            popup.classList.remove("popup-show");
-            popup.classList.remove("modal-error");
-            overlay.classList.remove("page-overlay-show");
-        }
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("popup-show")) {
+      popup.classList.remove("popup-show");
+      popup.classList.remove("modal-error");
+      overlay.classList.remove("page-overlay-show");
     }
+  }
 });
